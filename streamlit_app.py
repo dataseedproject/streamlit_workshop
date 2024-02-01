4e6ad61e71671600526bd67e0bcf6f1a52a67172
dataseedproject <157048939+dataseedproject@users.noreply.github.com>
2024-02-01 21:31:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Yummy Breakfast Menu')
+streamlit.text('Omlett with fried paprikas') 
+streamlit.text('Granola with yogurt and fruits')
+streamlit.text('Oat-pankakes with no sugar')
