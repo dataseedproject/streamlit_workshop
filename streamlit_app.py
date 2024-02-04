0d88ad75753f69cdb834b0fcdcfd08a176774389
dataseedproject <157048939+dataseedproject@users.noreply.github.com>
2024-02-04 20:05:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,6 @@ except URLError as e:
   streamlit.error() 
 
 
-
-streamlit.stop()
-
 my_cnx =  snowflake.connector.connectmy_cnx =  snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from  fruit_load_list")
