1148235a44c0ba62bb3001bbc91060df70324174
dataseedproject <157048939+dataseedproject@users.noreply.github.com>
2024-02-04 20:05:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,14 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit )
+def insert_row_snowflake(new_fruit):
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute(" insert into fruit_load_list values ('" + new_fruit + "')")
+          return "Thanks for adding " + new_fruit
 
-my_cur.execute(" insert into fruit_load_list values ('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?' )
+if streamlit.button('Add A Fruit to the List'): 
+     back_from_function = insert_row_snowflake(add_my_fruit)
+     streamlit.text(back_from_function)
+
+
