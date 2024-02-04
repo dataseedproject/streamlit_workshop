18223446b43a0cf39fb6fb73b151e0ee284a85a6
dataseedproject <157048939+dataseedproject@users.noreply.github.com>
2024-02-04 19:41:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('My Moms New Healthy Dinner')
